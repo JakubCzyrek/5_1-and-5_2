53b024d48cc3ad6a8a381d7aa126a8b8fc164cce
Jakub Czyrek <101180591+JakubCzyrek@users.noreply.github.com>
2022-06-14 10:00:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+(function () {
+  const example = document.getElementById('example')
+  const cw1 = document.getElementById('cw1')
+  const cw2 = document.getElementById('cw2')
+  const cw3 = document.getElementById('cw3')
+  const answer = document.getElementById('answer')
+
+  example.addEventListener("click", function () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        console.log(array)
+        answer.innerHTML = JSON.stringify(array);
+      })
+  })
+
+  cw1.addEventListener("click", function () {
+              answer.txtContent="";
+              answer.innerHTML = "Loading...............";
+          fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
+      .then(response => response.json())
+      .then(function(dane) {
+        answer.textContent="";
+        wyswietlDane(dane);
+      })
+  function wyswietlDane(dane){
+    for(var i=0 ; i<dane.length; i++){
+      var div = document.createElement("div");
+      div.innerHTML=
+        '<u>User ID: </u>'+dane[i].userId+
+        '<br><u>ID:</u>'+dane[i].id+
+        '<br><u>Title:</u>'+dane[i].title+
+        '<br><u>Body: </u>'+dane[i].Body+
+        '<hr>';
+      answer.appendChild(div);
+      }
+    
+    }
+   })
+  cw2.addEventListener("click", function () {
+    //TODO implement it
+  })
+
+  cw3.addEventListener("click", function () {
+    //TODO implement it
+  })
+
+})();
